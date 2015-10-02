d395def4c98a615abf2591ddc2ce353b027f44d3
rz41k <razorback524ch@yahoo.co.jp>
2015-10-02 21:23:12 +0900

    謎のプロローグの追加

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+    RectTransform rect;
+    // Use this for initialization
+    void Start() {
+        rect = GetComponent<RectTransform>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        rect.transform.position += rect.transform.up * 0.5f;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel(SceneName.prototype01_test);
+        }
+    }
+    
+}
